6f54a67ab8258500705a7eacca2ca68db2341abd
usuario <usuario@B301-02.dai.red>
2013-11-13 20:21:30 +0100

    1

--- UT3ControlFlujo/src/Ejercicios/Ejercicio6.java
+++ UT3ControlFlujo/src/Ejercicios/Ejercicio6.java
@@ -2,61 +2,13 @@ package Ejercicios;
 import java.util.Scanner;
 public class Ejercicio6 {
 /*
- * El juego de piedra/papel/tijera. Haz un programa para jugar a piedra/papel/tijera 
- * con el ordenador. Solo una ronda por ejecución.
- *  
- * para obtener un número aleatorio en Java entre 1 y 3
- * resultado = (int )(Math.random() * 3 + 1);
+ *Escribe un programa que pida el ph de una solución y muestre el mensaje de si la solución es ácida
+ *(ph < 7.0) o peligrósamente ácida: ph < 3.0).
  */
 	public static void main(String[] args) {
-		Scanner teclado = new Scanner(System.in);
-		String jugada = "";
-		int maquina = 0;
-		String resultado = "";
 		
-		maquina = (int )(Math.random() * 3 + 1);
-		switch (maquina) {
-		case 1: resultado = "piedra";
-			break;
-		case 2: resultado = "papel";
-			break;
-		case 3: resultado = "tijera";	
-			break;
-		default:
-			break;
-		}
-		System.out.println("Dime qué quieres sacar en tu jugada (piedra, papel o tijera): ");
-		jugada = teclado.nextLine();
-		teclado.close();
-		if (jugada.toLowerCase().equals("piedra")) {
-			if (resultado.equals("piedra")) {
-				System.out.println("Empate");	
-			}else if (resultado.equals("papel")) {
-				System.out.println("Pierdes");
-			}else{
-				System.out.println("Ganas");
-			}	
-		}
-		if (jugada.toLowerCase().equals("papel")) {
-			if (resultado.equals("piedra")) {
-				System.out.println("Ganas");
-			}else if (resultado.equals("papel")) {
-				System.out.println("Empate");
-			}else{
-				System.out.println("Pierdes");
-			}
-			
-		}
-		if (jugada.toLowerCase().equals("tijera")){
-			if (resultado.equals("piedra")) {
-				System.out.println("Pierdes");
-			}else if (resultado.equals("papel")) {
-				System.out.println("Ganas");
-			}else{
-				System.out.println("Empate");
-			}
-		}
-		System.out.println("Has elegido " + jugada + " y la máquina ha elegido " + resultado + ".");
+
+	
 	}
 
 }
