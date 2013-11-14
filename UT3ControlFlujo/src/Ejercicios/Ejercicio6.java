fa5adb87975fcb0021f4dcdb347674f4b08b5f00
usuario <usuario@B301-02.dai.red>
2013-11-14 16:25:52 +0100

    1

--- UT3ControlFlujo/src/Ejercicios/Ejercicio6.java
+++ UT3ControlFlujo/src/Ejercicios/Ejercicio6.java
@@ -6,6 +6,17 @@ public class Ejercicio6 {
  *(ph < 7.0) o peligrósamente ácida: ph < 3.0).
  */
 	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		double ph = 0;
+		teclado.close();
+		
+		if (ph < 7.0 && ph > 3.0) {
+			System.out.println("La solución es ácida.");
+		}else if (ph < 3.0) {
+			System.out.println("La solución es peligrosamente ácida.");
+		}else{
+			System.out.println("El ph está bien.");
+		}
 		
 
 	
