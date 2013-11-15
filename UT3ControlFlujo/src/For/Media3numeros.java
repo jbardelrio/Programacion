ba4f943a2df836c62f61cb067e5c745b99701e3e
usuario <usuario@B301-02.dai.red>
2013-11-15 17:58:30 +0100

    1

---
+++ UT3ControlFlujo/src/For/Media3numeros.java
@@ -0,0 +1,19 @@
+package For;
+import java.util.Scanner;
+public class Media3numeros {
+//Pedir 3 números y hacer la media
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		int suma = 0;
+		double media;
+		for (int i = 1; i <= 3; i++) {
+			System.out.println("Dime un numero");
+			int numero;
+			numero = teclado.nextInt();
+			suma += numero;
+		}
+		media = suma / 3;
+		System.out.println("La media es: " + media);
+	}
+
+}
