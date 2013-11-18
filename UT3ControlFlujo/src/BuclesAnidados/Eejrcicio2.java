580c95798116cab0eb4735c1c42afbe65591a154
jbardelrio <jbardelrio@gmail.com>
2013-11-18 12:13:10 +0100

    1

---
+++ UT3ControlFlujo/src/BuclesAnidados/Eejrcicio2.java
@@ -0,0 +1,27 @@
+package BuclesAnidados;
+
+import java.util.Scanner;
+
+public class Eejrcicio2 {
+//Modifica el ejercicio de la tabla de multiplicar de forma que el usuario
+//vaya introduciendo números y se vayan imprimiendo las tablas correspondientes. 
+//El programa termina cuando se introduce un número no esté entre 1 y 10.
+	public static void main(String[] args) {
+		int numero;
+		Scanner teclado = new Scanner(System.in);
+		
+		do {
+			
+		int linea;
+		System.out.println("Dime de qué número quieres calcular la tabla de multiplicar");
+		numero = teclado.nextInt();
+		
+		for (linea = 1; linea <= 10; linea++) {
+			System.out.println(numero + " x " + linea + " = " + (numero*linea));
+		}
+		} while (numero >= 1 && numero <= 10);
+		teclado.close();
+
+	}
+
+}
--- UT3ControlFlujo/src/BuclesAnidados/Ejercicio1.java
+++ UT3ControlFlujo/src/BuclesAnidados/Ejercicio1.java
@@ -5,21 +5,35 @@ public class Ejercicio1 {
 //de dicho número. Hay que validar que el número insertado sea positivo 
 //(incluido el 0). Si no es así, se volverá a pedir dicho número. 
 //Posteriormente, el programa le preguntará al usuario si quiere insertar
-//otro número, con un mensaje como este:"¿Quiere insetar otro número [s/n]?".
+//otro número, con un mensaje como este:"¿Quiere insertar otro número [s/n]?".
 //Debes validar también esta respuesta.
 	public static void main(String[] args) {
 		int numero;
-		int multiplicacion = 1;
+		String respuesta = "s";
 		Scanner teclado = new Scanner(System.in);
-		System.out.println("Introduce el número: ");
-		numero = teclado.nextInt();
+		
 		do {
-			System.out.println("El número intruducido es negativo, introduce otro nuevo: ");
-		} while (numero < 0);
-		for (int i = 1; i < numero + 1; i++) {
-			multiplicacion *= i;
-		}
-		System.out.println("El factorial de " + numero + " es: " + multiplicacion);
+			int multiplicacion = 1;
+			System.out.println("Introduce el número: ");
+			numero = teclado.nextInt();
+			
+				if (numero < 0) {
+					System.out.println("El número intruducido es negativo, introduce otro nuevo: ");
+					numero = teclado.nextInt();
+				}
+				
+				for (int i = 1; i < numero + 1; i++) {
+					multiplicacion *= i;
+				}
+				
+			System.out.println("El factorial de " + numero + " es: " + multiplicacion);
+			System.out.println("¿Quiere insertar otro número [s/n]?");
+			respuesta = teclado.next();
+			
+		} while (respuesta.equals("s") || respuesta.equals("S"));
+
+		System.out.println("Fin del programa.");
+		teclado.close();
 	}
 
 }
