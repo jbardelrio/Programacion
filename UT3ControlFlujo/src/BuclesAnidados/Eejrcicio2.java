8d8da05712ba34931f5950683d1e1bf17862be36
jbardelrio <jbardelrio@gmail.com>
2013-11-18 12:15:51 +0100

    1

--- UT3ControlFlujo/src/BuclesAnidados/Eejrcicio2.java
+++ UT3ControlFlujo/src/BuclesAnidados/Eejrcicio2.java
@@ -12,15 +12,18 @@ public class Eejrcicio2 {
 		
 		do {
 			
-		int linea;
-		System.out.println("Dime de qué número quieres calcular la tabla de multiplicar");
-		numero = teclado.nextInt();
-		if (numero >= 1 && numero <= 10) {
-			for (linea = 1; linea <= 10; linea++) {
-				System.out.println(numero + " x " + linea + " = " + (numero*linea));
-		}
-		}
+			int linea;
+			System.out.println("Dime de qué número quieres calcular la tabla de multiplicar");
+			numero = teclado.nextInt();
+			
+				if (numero >= 1 && numero <= 10) {
+					for (linea = 1; linea <= 10; linea++) {
+						System.out.println(numero + " x " + linea + " = " + (numero*linea));
+					}
+				}
+				
 		} while (numero >= 1 && numero <= 10);
+		
 		System.out.println("Fin del programa.");
 		teclado.close();
 
