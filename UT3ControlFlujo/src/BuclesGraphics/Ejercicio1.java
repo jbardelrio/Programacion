5988423b78c6c0645fbecf1df2c88defe9f6d848
usuario <usuario@B301-02.dai.red>
2013-11-20 20:31:28 +0100

    1

---
+++ UT3ControlFlujo/src/BuclesGraphics/Ejercicio1.java
@@ -0,0 +1,10 @@
+package BuclesGraphics;
+
+public class Ejercicio1 {
+//Semaforo: El semáforo tiene un ciclo y vuelve a verde después del rojo, indefinidamente, hasta que cierre la aplicación.
+	public static void main(String[] args) {
+		
+
+	}
+
+}
