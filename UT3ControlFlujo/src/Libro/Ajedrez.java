331fa200a2e9b5b496bed9dca7bee091fe047627
jbardelrio <jbardelrio@gmail.com>
2013-12-05 01:09:31 +0100

    1

---
+++ UT3ControlFlujo/src/Libro/Ajedrez.java
@@ -0,0 +1,33 @@
+package Libro;
+
+//Leer.class debe estar en la carpeta especificada por CLASSPATH
+public class Ajedrez
+{
+// Imprimir un tablero de ajedrez.
+public static void main(String[] args)
+{
+ int falfil, calfil; // posición inicial del alfil
+ int fila, columna;  // posición actual del alfil
+ 
+ System.out.println("Posición del alfil:");
+ System.out.print("  fila    "); falfil = 0;
+ System.out.print("  columna "); calfil = 0;
+ System.out.println(); // dejar una línea en blanco
+ 
+ // Pintar el tablero de ajedrez
+ for (fila = 1; fila <= 8; fila++)
+ {
+   for (columna = 1; columna <= 8; columna++)
+   {
+     if ((fila + columna == falfil + calfil) ||
+        (fila - columna == falfil - calfil))
+       System.out.print("* ");
+     else if ((fila + columna) % 2 == 0)
+       System.out.print("B ");
+     else
+       System.out.print("N ");
+   }
+   System.out.println(); // cambiar de fila
+ }
+}
+}
