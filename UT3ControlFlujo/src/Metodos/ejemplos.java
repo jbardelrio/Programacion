313d7d9bad93b6b66e45d353c644276984edfa12
usuario <usuario@B301-02.dai.red>
2013-12-11 20:25:04 +0100

    1

---
+++ UT3ControlFlujo/src/Metodos/ejemplos.java
@@ -0,0 +1,15 @@
+package Metodos;
+
+public class ejemplos {
+
+	public static void main(String[] args) {
+		imprime();
+		imprime();
+		System.out.println("Adiós");
+
+	}
+	//void: si lo lleva no devuelve nada, es decir, no lleva un RETURN dentro
+	public static void imprime() {
+		System.out.println("Hola método");
+	}
+}
