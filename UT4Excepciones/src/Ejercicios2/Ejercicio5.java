63ec401bc3a4bb24b4d39ec2007468729e3f0876
usuario <usuario@B301-02.dai.red>
2014-01-30 17:59:40 +0100

    1

--- UT4Excepciones/src/Ejercicios2/Ejercicio5.java
+++ UT4Excepciones/src/Ejercicios2/Ejercicio5.java
@@ -4,11 +4,82 @@ package Ejercicios2;
 //no haya llegado a insertar ningún número, y habrá que visualizar el mensaje que corresponda.
 //Hay que usar un método que pida un número y que controle el posible error que pudiera producirse si el usuario
 //inserta letras. El método visualizará un mensaje informando del error.
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Ejercicio5 {
+        
+        private static Scanner teclado;
+        
+        public static double leerNumero() {
+                
+                teclado = new Scanner(System.in);
+                double numero = 0;
+                
+                System.out.print("Introduzca un nÃºmero [pulse * para finalizar]: ");
+                
+                try {
+                        
+                        numero = teclado.nextDouble();
+                }
+                
+                catch ( InputMismatchException entradaInvalida ) {
+                        
+                        throw entradaInvalida;
+                }
+                
+                return numero;
+        }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        public static void main(String[] args) {
 
-	}
+                double[] numeros = new double[40];
+                int contadorNumeros = 0;
+                String letraIntroducida = "";
+                boolean continuarLeyendo = true;
+
+                int i = 0;
+                do {
+                
+                        try {
+                                
+                                for (; i < numeros.length; ++i ) {
+                                
+                                        numeros[i] = leerNumero();
+                                        ++contadorNumeros;                                
+                                }
+                        }
+                        
+                        catch (InputMismatchException entradaInvalida ) {
+                                
+                                letraIntroducida = teclado.next();
+                                
+                                if (letraIntroducida.equals("*") && contadorNumeros == 0) {
+                                        
+                                        System.out.println("No ha introducido ningÃºn nÃºmero.");
+                                        System.exit(-1);
+                                
+                                } else if (letraIntroducida.equals("*") && contadorNumeros > 0) {
+                                        
+                                        System.out.println("Fin de lectura. NÃºmeros leÃ­dos: " + contadorNumeros);
+                                        continuarLeyendo = false;
+                                
+                                } else {
+                                        
+                                        System.out.println("Debe introducir nÃºmeros. IntÃ©ntelo de nuevo.");
+                                }        
+                        }
+                
+                } while ( continuarLeyendo );
+                
+                double mayor = numeros[0];
+                
+                for ( int j = 1; j < contadorNumeros; ++j ) {
+                                
+                        if ( numeros[j] >= mayor ) mayor = numeros[j];                                        
+                }
+                        
+                System.out.println("El MAYOR es: " + mayor );
+        }
 
 }
