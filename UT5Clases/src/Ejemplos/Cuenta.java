4762818787846b8fc89fe1f8bb19af0f46d94050
usuario <usuario@B301-02.dai.red>
2014-02-05 20:29:11 +0100

    1

--- UT5Clases/src/Ejemplos/Cuenta.java
+++ UT5Clases/src/Ejemplos/Cuenta.java
@@ -1,8 +1,18 @@
 package Ejemplos;
 
 public class Cuenta {
-
-	public double  {
-		
+	private double saldo=0;
+	
+	public void ingresar(double cantidad) {
+		saldo+=cantidad;
+	}
+	public void sacar(double cantidad) {
+		saldo-=cantidad;
+		if (saldo<0) {
+			System.out.println("No hay pasta.");
+		}
+	}
+	public double getSaldo(){
+		return saldo;
 	}
 }
--- UT5Clases/src/Ejemplos/Ejemplo3.java
+++ UT5Clases/src/Ejemplos/Ejemplo3.java
@@ -3,9 +3,11 @@ package Ejemplos;
 public class Ejemplo3 {
 
 	public static void main(String[] args) {
-		Cuenta ingresar, sacar;
-		ingresar = new Cuenta();
-
+		Cuenta  mi_cuenta;
+		mi_cuenta=new Cuenta();
+		mi_cuenta.ingresar(1000);
+		
+		System.out.println(mi_cuenta.getSaldo());
 	}
 
 }
