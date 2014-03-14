acacaaf4b034d13195b433ecaa9dc5096738facf
jbardelrio <jbardelrio@gmail.com>
2014-03-14 17:52:18 +0100

    1

---
+++ UT6Almacenamientoo/src/ejercicios/Ejercicio1.java
@@ -0,0 +1,11 @@
+package ejercicios;
+//Ejercicio 1 Realiza un programa que calcule los 6 números para rellenar una 
+//quiniela primitiva controlando que no se repita ninguno de ellos.
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
