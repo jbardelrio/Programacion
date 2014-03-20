662f32b2a0570c29fcdbb3d46608d169c049f0a9
usuario <usuario@B301-02.dai.red>
2014-03-20 17:39:09 +0100

    1

--- UT6Almacenamientoo/src/vectoresymatrices/Ejercicio2.java
+++ UT6Almacenamientoo/src/vectoresymatrices/Ejercicio2.java
@@ -9,8 +9,16 @@ public class Ejercicio2 {
 
 	
 	public static void main(String[] args) {
-	
+		
+		int[][] notas={{5, 6, 9, 7},{5, 5, 5, 5, 5, 5, 5},{6, 6, 5},{8, 7}};
 
+		for (int i = 0; i < notas.length; i++) { 
+			for (int j = 0; j < notas[i].length; j++) { 
+				System.out.print(notas[i][j]+ " ");
+			}
+			System.out.println();
+		}
+		
 	}
 
 }
