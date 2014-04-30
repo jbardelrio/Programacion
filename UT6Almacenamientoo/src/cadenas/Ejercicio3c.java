506daff8b4f49c35b4b1a0ae1fb44b45761735b8
jbardelrio <jbardelrio@gmail.com>
2014-04-30 20:05:10 +0200

    1

---
+++ UT6Almacenamientoo/src/cadenas/Ejercicio3c.java
@@ -0,0 +1,11 @@
+package cadenas;
+//Ejercicio 3. Dada una frase, que puede contener caracteres, espacios en blanco, puntos y comas, contar las palabras que contiene. 
+//Por ejemplo "El perro de San Roque. Fin." devuelve 6.
+public class Ejercicio3c {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
