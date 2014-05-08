9aebee8a34b4d0c709d89b55b31cd306cc31f27f
usuario <usuario@B301-02.dai.red>
2014-05-08 18:05:55 +0200

    1

--- UT7ClasesAvanzadoo/src/ejerciciosInicialesHerencia5/Mamifero.java
+++ UT7ClasesAvanzadoo/src/ejerciciosInicialesHerencia5/Mamifero.java
@@ -5,7 +5,7 @@ public class Mamifero extends Animal {
 	private String nombreraza;
 	
 	public Mamifero(double peso, String fechanacimiento, String nombrecliente, String direccion) {
-		// TODO Auto-generated constructor stub
+	
 	}
 
 	public static void main(String[] args) {
